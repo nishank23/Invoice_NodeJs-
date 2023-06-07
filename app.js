24801bb2cfc107fb24946219c84a7dd06baaa78d
nishank100 <bansalnishank4gmail.com>
2023-06-07 23:55:08 +0530

    Firebase admin integrated

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
 const express = require('express');
-import {admin} from "../Invoice_manager_backend/config/firebase";
 
 const app = express();
 app.use(express.json());
 
 
 
-
-app.post('/login/google', async (req, res) => {
-    const { idToken } = req.body;
-
-    try {
-        console.log("data")// Verify the authenticity of the Google sign-in credentials
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-
-        // Retrieve the user's unique identifier (UID) and other necessary information
-        const uid = decodedToken.uid;
-        const email = decodedToken.email;
-        // Add other required user information as needed
-
-        // Create a custom token for the user
-        const customToken = await admin.auth().createCustomToken(uid);
-
-        // Return the custom token as the response
-        res.json({ token: customToken });
-    } catch (error) {
-        console.error('Google sign-in error:', error);
-        res.status(500).json({ error: 'Google sign-in failed' });
-    }
-});
 
 // const morgan = require('morgan');
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
+import {admin} from "../Invoice_manager_backend/config/firebase";
 
 const connectDB = require('./config/database');
 
@@ -15,6 +16,30 @@ app.get('/', (req, res) => {
 
 
 });
+
+app.post('/login/google', async (req, res) => {
+    const { idToken } = req.body;
+
+    try {
+        console.log("data")// Verify the authenticity of the Google sign-in credentials
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+
+        // Retrieve the user's unique identifier (UID) and other necessary information
+        const uid = decodedToken.uid;
+        const email = decodedToken.email;
+        // Add other required user information as needed
+
+        // Create a custom token for the user
+        const customToken = await admin.auth().createCustomToken(uid);
+
+        // Return the custom token as the response
+        res.json({ token: customToken });
+    } catch (error) {
+        console.error('Google sign-in error:', error);
+        res.status(500).json({ error: 'Google sign-in failed' });
+    }
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
