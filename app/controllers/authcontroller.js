efd60e101abfde412ed32f2026d50ba19dfb8025
nishank100 <bansalnishank4gmail.com>
2023-06-07 23:52:24 +0530

    Firebase admin integrated

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -1,31 +1,4 @@
 import app from "../../app";
-import {admin} from "../../config/firebase";
-
-
-
-
-app.post('/login/google', async (req, res) => {
-    const { idToken } = req.body;
-
-    try {
-        console.log("data")// Verify the authenticity of the Google sign-in credentials
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-
-        // Retrieve the user's unique identifier (UID) and other necessary information
-        const uid = decodedToken.uid;
-        const email = decodedToken.email;
-        // Add other required user information as needed
-
-        // Create a custom token for the user
-        const customToken = await admin.auth().createCustomToken(uid);
-
-        // Return the custom token as the response
-        res.json({ token: customToken });
-    } catch (error) {
-        console.error('Google sign-in error:', error);
-        res.status(500).json({ error: 'Google sign-in failed' });
-    }
-});
 
 
 app.post('/login/facebook', async (req, res) => {
