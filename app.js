40051f3801a945d0f3b54124c5b42fe76bfa1ad1
nishank100 <bansalnishank4gmail.com>
2023-06-07 23:45:12 +0530

    Firebase admin integrated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.json());
+app.use
 // const morgan = require('morgan');
 
 // const taskrouter = require('./routes/taskroutes');
