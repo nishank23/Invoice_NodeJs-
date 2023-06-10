4ecf7aba3e91c7cc427feb037daa2d0e02796cf3
Nishankban <nishank_bansal@querosys.com>
2023-06-10 20:43:41 +0530

    Updates..a

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ dotenv.config({ path: './config.env' });
 const uri = `mongodb+srv://myadmin:admin@studentcluster.nxhqg6z.mongodb.net/?retryWrites=true&w=majority`;
 
 function connectDB() {
+
+    console.log("data")
+
     return mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
