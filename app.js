037c3fde3331906c5a6e0ae8da268687464a4627
Nishankban <nishank_bansal@querosys.com>
2023-06-12 00:03:12 +0530

    fixed fcm issue testing verify through mail

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ app.use(express.json());
 
 const authrouter = require('./app/routes/authroutes');
 
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 
 app.use('/api/v1',authrouter);
