ea1b5e441af361492181f1b3622ac7f7de0f084f
nishank23 <bansalnishank4@gmail.com>
2023-12-05 08:26:10 +0200

    changes

--- src/models/ProductModels/product.js
+++ src/models/ProductModels/product.js
@@ -19,7 +19,7 @@ const productSchema = new mongoose.Schema(
         },
         description: {
             type: String,
-            required: true
+            required: false
         },
         images: [String],
         userId: {
